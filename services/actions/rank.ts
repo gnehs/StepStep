820bde232b7af35f3fe2ca6ec243ef4f24c0f179
gnehs <gnehs@gnehs.net>
2024-05-12 15:48:34 +0800

    added rank try catch

--- services/actions/rank.ts
+++ services/actions/rank.ts
@@ -1,56 +1,62 @@
 "use server";
 import prisma from "@/services/prisma";
 export async function getRank() {
-  // +0800 is the timezone offset for Taipei
-  // get today 00:00:00 in Taipei timezone
-  let today = new Date();
-  today.setHours(0, 0, 0, 0);
+  try {
+    // +0800 is the timezone offset for Taipei
+    // get today 00:00:00 in Taipei timezone
+    let today = new Date();
+    today.setHours(0, 0, 0, 0);
 
-  let historyRecords = [];
-  for (let i = 0; i < 7; i++) {
-    let date = new Date(today);
-    date.setDate(date.getDate() - i);
-    let nextDate = new Date(date);
-    nextDate.setDate(nextDate.getDate() + 1);
-    let records = await prisma.record.groupBy({
-      by: ["userId"],
-      _sum: {
-        steps: true,
-        distance: true,
-        energy: true,
-      },
-      where: {
-        timestamp: {
-          gte: date,
-          lt: nextDate,
+    let historyRecords = [];
+    for (let i = 0; i < 7; i++) {
+      let date = new Date(today);
+      date.setDate(date.getDate() - i);
+      let nextDate = new Date(date);
+      nextDate.setDate(nextDate.getDate() + 1);
+      let records = await prisma.record.groupBy({
+        by: ["userId"],
+        _sum: {
+          steps: true,
+          distance: true,
+          energy: true,
         },
-      },
-    });
+        where: {
+          timestamp: {
+            gte: date,
+            lt: nextDate,
+          },
+        },
+      });
 
-    records = records.sort((a, b) => (b._sum.steps ?? 0) - (a._sum.steps ?? 0));
-    records = records.slice(0, 10);
-    let parsedRecords = await Promise.all(
-      records.map(async (record) => {
-        let user = await prisma.user.findUnique({
-          where: {
-            id: record.userId,
-          },
-          select: {
-            id: true,
-            name: true,
-          },
-        });
-        return {
-          ...record._sum,
-          user,
-        };
-      })
-    );
-    historyRecords.push({
-      date,
-      records: parsedRecords,
-    });
+      records = records.sort(
+        (a, b) => (b._sum.steps ?? 0) - (a._sum.steps ?? 0)
+      );
+      records = records.slice(0, 10);
+      let parsedRecords = await Promise.all(
+        records.map(async (record) => {
+          let user = await prisma.user.findUnique({
+            where: {
+              id: record.userId,
+            },
+            select: {
+              id: true,
+              name: true,
+            },
+          });
+          return {
+            ...record._sum,
+            user,
+          };
+        })
+      );
+      historyRecords.push({
+        date,
+        records: parsedRecords,
+      });
+    }
+
+    return historyRecords;
+  } catch (e) {
+    return [];
   }
-
-  return historyRecords;
 }
