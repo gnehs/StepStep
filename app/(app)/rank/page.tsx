f687d9ecd29d5ca7359ba4257891c50776a6a6f7
gnehs <gnehs@gnehs.net>
2024-05-12 14:46:15 +0800

    rank page use ssr

--- app/(app)/rank/page.tsx
+++ app/(app)/rank/page.tsx
@@ -1,34 +1,9 @@
-"use client";
 import Container from "@/components/Container";
 import PageTitle from "@/components/PageTitle";
 import SectionTitle from "@/components/SectionTitle";
 import { getRank } from "@/services/actions/rank";
-import { useEffect, useState } from "react";
-export default function Rank() {
-  const [rank, setRank] = useState<
-    {
-      date: Date;
-      records: {
-        steps: number;
-        distance: number;
-        energy: number;
-        userId: string;
-        user: {
-          id: string;
-          name: string;
-          email: string;
-        };
-      }[];
-    }[]
-  >([]);
-  async function getData() {
-    let res = await getRank();
-
-    setRank(res as any);
-  }
-  useEffect(() => {
-    getData();
-  }, []);
+export default async function Rank() {
+  const rank = await getRank();
   return (
     <Container>
       <PageTitle>排行榜</PageTitle>
@@ -38,15 +13,15 @@ export default function Rank() {
           <div key={item.date.toLocaleDateString()}>
             <SectionTitle>{item.date.toLocaleDateString()}</SectionTitle>
             {item.records.map((item, index: number) => (
-              <div key={item.userId}>
-                <div className="bg-white rounded-lg p-2 my-2 flex justify-between gap-2 items-center">
+              <div key={item.user.id}>
+                <div className="bg-white rounded-lg p-2 my-2 flex justify-between gap-2 items-center shadow-sm">
                   <p className="font-bold">{item.user.name}</p>
-                  <p>{item.steps.toLocaleString()} 步</p>
+                  <p>{item.steps?.toLocaleString() ?? 0} 步</p>
                 </div>
               </div>
             ))}
             {item.records.length === 0 && (
-              <div className="bg-white rounded-lg p-2 my-1 flex justify-between gap-2 items-center text-gray-400">
+              <div className="bg-white rounded-lg p-2 my-2 flex justify-between gap-2 items-center text-gray-400 shadow-sm">
                 暫無資料
               </div>
             )}
