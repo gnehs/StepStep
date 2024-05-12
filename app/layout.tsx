b6333455e9586ffbba588291228d545fe94a322e
gnehs <gnehs@gnehs.net>
2024-05-12 15:32:49 +0800

    updated font

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="zh-TW">
+      <head>
+        <link rel="preconnect" href="https://rsms.me/" />
+        <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin=""
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@100..900&display=swap"
+          rel="stylesheet"
+        />
+      </head>
       <body className="bg-[#f2f2f2] text-[#111]">{children}</body>
     </html>
   );
