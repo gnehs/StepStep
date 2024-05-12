ae7451ec4580dbc7b21218f6880180e5b2c9e4c7
gnehs <gnehs@gnehs.net>
2024-05-12 16:04:14 +0800

    added icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,7 @@ export default function RootLayout({
   return (
     <html lang="zh-TW">
       <head>
+        {/* font */}
         <link rel="preconnect" href="https://rsms.me/" />
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
@@ -25,6 +26,9 @@ export default function RootLayout({
           href="https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@100..900&display=swap"
           rel="stylesheet"
         />
+        {/* icon */}
+        <link rel="icon" href="/logo-favicon.svg" type="image/svg+xml" />
+        <link rel="icon" href="/logo.png" type="image/png" />
       </head>
       <body className="bg-[#f2f2f2] text-[#111]">{children}</body>
     </html>
