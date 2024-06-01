8b4d3abb99b469729c6366272000ecc6654c44da
gnehs <gnehs@gnehs.net>
2024-06-01 17:24:45 +0800

    added history page

---
+++ app/(app)/history/page.tsx
@@ -0,0 +1,13 @@
+import Container from "@/components/Container";
+import PageTitle from "@/components/PageTitle";
+import SectionTitle from "@/components/SectionTitle";
+export default function HistoryPage() {
+  return (
+    <Container>
+      <PageTitle>進階分析</PageTitle>
+      <SectionTitle>今日統計</SectionTitle>
+      <SectionTitle>步步時間圖</SectionTitle>
+      <SectionTitle>步步時間圖</SectionTitle>
+    </Container>
+  );
+}
