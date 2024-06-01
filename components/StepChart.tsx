41cada9c90c97ad006c78fadeeb1ebcbb7a0c623
gnehs <gnehs@gnehs.net>
2024-06-01 20:33:42 +0800

    updated StepChart

--- components/StepChart.tsx
+++ components/StepChart.tsx
@@ -9,20 +9,24 @@ export default function StepChart({ data }: { data: Record[] }) {
     }),
     步數: record.steps,
   }));
-
   return (
-    <>
-      <ResponsiveBar
-        data={chartData}
-        keys={["步數"]}
-        indexBy="time"
-        margin={{ top: 30, right: 0, bottom: 0, left: 0 }}
-        indexScale={{ type: "band", round: true }}
-        enableGridY={false}
-        enableLabel={false}
-        ariaLabel="Steps bar chart"
-        enableTotals
-      />
-    </>
+    <ResponsiveBar
+      data={chartData}
+      keys={["步數"]}
+      indexBy="time"
+      margin={{ top: 30, right: 0, bottom: 0, left: 0 }}
+      indexScale={{ type: "band", round: true }}
+      enableGridY={false}
+      enableLabel={false}
+      ariaLabel="Steps bar chart"
+      enableTotals
+      valueFormat={(val) => val.toLocaleString()}
+      tooltip={({ indexValue, value }) => (
+        <div className="rounded-xl bg-white px-2 py-1 text-sm shadow-lg">
+          <span className="mr-1 text-gray-500"> {indexValue}</span>
+          <span className="text-gray-700"> {value.toLocaleString()} 步</span>
+        </div>
+      )}
+    />
   );
 }
