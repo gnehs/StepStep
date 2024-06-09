6dda9631714eb8d8b3ec2b95b570645886fa13ab
gnehs <gnehs@gnehs.net>
2024-06-10 00:24:57 +0800

    added badge data

---
+++ data/badges.ts
@@ -0,0 +1,73 @@
+export default [
+  {
+    id: "first-step",
+    name: "第一步",
+    description: "成功在餅餅踏踏同步第一步，這是你的一小步，也是最重要的一步",
+    icon: "👣",
+  },
+  {
+    id: "to-the-moon",
+    name: "登月",
+    description: "累積達 384,400 公里",
+    icon: "🌕",
+  },
+  {
+    id: "tpe-to-khh",
+    name: "北高來回也是小事",
+    description: "累積達 352.3 公里",
+    icon: "🚄",
+  },
+  {
+    id: "contributor",
+    name: "貢獻者",
+    description: "協助維護餅餅踏踏",
+    icon: "🧑‍💻",
+  },
+  {
+    id: "champion-of-steps",
+    name: "步步冠軍",
+    description: "在排行榜中排名第一",
+    icon: "🏆",
+  },
+  {
+    id: "full-house",
+    name: "步步大滿貫",
+    description: "在排行榜連續一週中排名第一",
+    icon: "🎉",
+  },
+  {
+    id: "daywalker",
+    name: "晝行者",
+    description: "一天內在白天累積超過 15,000 步",
+    icon: "🌞",
+  },
+  {
+    id: "nightwalker",
+    name: "夜行者",
+    description: "一天內在晚間累積超過 15,000 步",
+    icon: "🌙",
+  },
+  {
+    id: "midnight-walker",
+    name: "午夜行者",
+    description: "在午夜時分累積超過 15,000 步",
+    icon: "🛌",
+  },
+  {
+    id: "country-jp",
+    name: "踏足日本",
+    description: "在日本同步餅餅踏踏",
+    icon: "🇯🇵",
+  },
+  {
+    id: "country-tw",
+    name: "踏足台灣",
+    description: "在台灣同步餅餅踏踏",
+    icon: "🇹🇼",
+  },
+] as {
+  id: string;
+  name: string;
+  description: string;
+  icon: string;
+}[];
