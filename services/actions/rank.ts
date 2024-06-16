a554820edcab94963e7aa60c5afcbff1699f4b6c
gnehs <gnehs@gnehs.net>
2024-06-17 00:27:09 +0800

    refactor rank function

--- services/actions/rank.ts
+++ services/actions/rank.ts
@@ -11,49 +11,52 @@ export async function getRank(year?: number, month?: number) {
 
   let historyRecords = [];
   for (let date of dates) {
-    let gte = new Date(year, month - 1, date);
-    let lt = new Date(year, month - 1, date + 1);
-    let records = await prisma.record.groupBy({
-      by: ["userId"],
-      _sum: {
-        steps: true,
-        distance: true,
-        energy: true,
-      },
-      where: {
-        timestamp: {
-          gte,
-          lt,
-        },
-      },
-    });
-    records = records.sort(
-      (a, b) => (b._sum.distance ?? 0) - (a._sum.distance ?? 0),
-    );
-    records = records.slice(0, 10);
-
-    let parsedRecords = await Promise.all(
-      records.map(async (record) => {
-        let user = await prisma.user.findUnique({
-          where: {
-            id: record.userId,
-          },
-          select: {
-            id: true,
-            name: true,
-          },
-        });
-        return {
-          ...record._sum,
-          user,
-        };
-      }),
-    );
     historyRecords.push({
-      date: gte,
-      records: parsedRecords,
+      date: new Date(year, month - 1, date),
+      records: await getRankByDay(year, month, date),
     });
   }
 
   return historyRecords;
+}
+export async function getRankByDay(year: number, month: number, date: number) {
+  let gte = new Date(year, month - 1, date);
+  let lt = new Date(year, month - 1, date + 1);
+  let records = await prisma.record.groupBy({
+    by: ["userId"],
+    _sum: {
+      steps: true,
+      distance: true,
+      energy: true,
+    },
+    where: {
+      timestamp: {
+        gte,
+        lt,
+      },
+    },
+  });
+  records = records.sort(
+    (a, b) => (b._sum.distance ?? 0) - (a._sum.distance ?? 0),
+  );
+  records = records.slice(0, 10);
+
+  let parsedRecords = await Promise.all(
+    records.map(async (record) => {
+      let user = await prisma.user.findUnique({
+        where: {
+          id: record.userId,
+        },
+        select: {
+          id: true,
+          name: true,
+        },
+      });
+      return {
+        ...record._sum,
+        user,
+      };
+    }),
+  );
+  return parsedRecords;
 }
