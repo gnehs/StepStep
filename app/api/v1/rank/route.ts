8bf76415775bcbabcc934f3f8d057cebc69a21fa
gnehs <gnehs@gnehs.net>
2024-06-17 00:34:56 +0800

    added rank API

---
+++ app/api/v1/rank/route.ts
@@ -0,0 +1,17 @@
+import { getRankByDay } from "@/services/actions/rank";
+import type { NextRequest } from "next/server";
+export async function GET(request: NextRequest) {
+  const date = request.nextUrl.searchParams.get("date");
+  // chcek date is YYYY-MM-DD format
+  if (!date || !/^\d{4}-\d{2}-\d{2}$/.test(date)) {
+    return new Response("Invalid date", { status: 400 });
+  }
+  const splitDate = date.split("-").map(Number);
+  console.log(splitDate);
+  const rank = await getRankByDay(splitDate[0], splitDate[1], splitDate[2]);
+  return new Response(JSON.stringify(rank), {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+}
